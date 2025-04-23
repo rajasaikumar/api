cd3474895a220c95e1532d38b888779a5ed1bba5
Saikumar <saikumar54722@gmail.com>
2025-04-23 05:47:38 -0700

    cors removed

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:8000", "http://127.0.0.1:8000"  , "http://localhost:3000",
+        "http://127.0.0.1:3000","https://your-cloudflare-page.pages.dev"],
     
     allow_credentials=True,
     allow_methods=["*"],
