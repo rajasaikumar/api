bc76dfbc40bf341b3bd3d3ab6f3e2038d8955387
Saikumar <saikumar54722@gmail.com>
2025-04-23 05:45:39 -0700

    cors

--- main.py
+++ main.py
@@ -13,8 +13,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:8000", "http://127.0.0.1:8000"  , "http://localhost:3000",
-        "http://127.0.0.1:3000","https://your-cloudflare-page.pages.dev"],
+    allow_origins=["*"],
     
     allow_credentials=True,
     allow_methods=["*"],
