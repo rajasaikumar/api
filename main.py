455815eaf10c41b41a7b93b2cde5ed18c0ea7369
Saikumar <saikumar54722@gmail.com>
2025-04-28 00:15:35 -0700

    changes

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5500", "http://127.0.0.1:5500", "http://localhost:8000", "http://127.0.0.1:8000"  , "http://localhost:3000",
-        "http://127.0.0.1:3000","https://c071c862.reactredux-d05.pages.dev"],
+        "http://127.0.0.1:3000"],
     
     allow_credentials=True,
     allow_methods=["*"],
